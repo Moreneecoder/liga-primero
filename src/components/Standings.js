137278e9399806b28434d198d697dbe86fe74f5f
Morenikeji Bello <caeserselassie@gmail.com>
2021-11-18 07:47:32 +0100

    Add Standings and Team components

---
+++ src/components/Standings.js
@@ -0,0 +1,7 @@
+const Standings = () => (
+  <div className="Standings">
+    <h1>League Table</h1>
+  </div>
+);
+
+export default Standings;
---
+++ src/components/Team.js
@@ -0,0 +1,7 @@
+const Team = () => (
+  <div className="Team">
+    <h1>Single Team Page</h1>
+  </div>
+);
+
+export default Team;
