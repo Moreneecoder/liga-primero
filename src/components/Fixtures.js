f1ae08715b3d992bf3583ebf80e6e2e387aff946
Morenikeji Bello <caeserselassie@gmail.com>
2021-11-23 20:38:53 +0100

    Tidy code

--- src/components/Fixtures.js
+++ src/components/Fixtures.js
@@ -5,11 +5,11 @@ import goalie from '../images/goalie.svg';
 const Fixtures = () => (
   <div className="Fixtures px-1 px-md-0 col-12 col-md-6 mx-auto text-center">
     <img className="mb-3" style={{ width: '50%' }} src={goalie} alt="not-found" />
-    <div className="alert alert-info p-2">
+    <h3 className="alert alert-info p-2">
       <span className="fw-bolder">Fixtures</span>
       {' '}
       Feature Coming Soon! In the Pipes...
-    </div>
+    </h3>
     <Link to="/" className="text-white btn btn-md btn-md-lg main-bg-color">&#8592; Go Back To standings</Link>
   </div>
 );
--- src/components/GoalChart.js
+++ src/components/GoalChart.js
@@ -5,11 +5,11 @@ import footballer from '../images/footballer.svg';
 const GoalChart = () => (
   <div className="GoalChart px-1 px-md-0 col-12 col-md-6 mx-auto text-center">
     <img className="mb-3" style={{ width: '50%' }} src={footballer} alt="not-found" />
-    <div className="alert alert-info p-2">
+    <h3 className="alert alert-info p-2">
       <span className="fw-bolder">Goal Chart</span>
       {' '}
       Feature Coming Soon! In the Pipes...
-    </div>
+    </h3>
     <Link to="/" className="text-white btn btn-md btn-md-lg main-bg-color">&#8592; Go Back To standings</Link>
   </div>
 );
