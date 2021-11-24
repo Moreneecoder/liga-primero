29ee5a1f19a8de3456fc5873ab82c2d1002b9c1a
Morenikeji Bello <caeserselassie@gmail.com>
2021-11-24 16:49:15 +0100

    Add tests for standingsReducer

---
+++ src/__test__/Reducers.test.js
@@ -0,0 +1,34 @@
+import standingsReducer from "../reducers/standings";
+
+const state = [];
+const action = {
+  type: 'REFRESH_TABLE',
+  latestTable: [
+    {
+        rank: '1',
+        team: 'Barcelona',
+        points: 37,
+    }
+  ]
+}
+
+describe('Reducers Tests', () => {
+
+  describe('standingsReducer Tests', () => {
+    test('returns the correct updated state', () => {
+        // console.log(standingsReducer(state, action));
+        expect(standingsReducer(state, action)[0]).toEqual({
+            rank: '1',
+            team: 'Barcelona',
+            points: 37,
+        });
+      });
+
+      test('returns empty array state when wrong action type is passed', () => {
+        action.type = 'SOMETHING_ELSE';
+        console.log(standingsReducer(state, action));
+        expect(standingsReducer(state, action)).toEqual([]);
+      });
+  });
+
+});
