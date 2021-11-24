a5102bbde2559fc966c173af691e113edad1d000
Morenikeji Bello <caeserselassie@gmail.com>
2021-11-24 19:45:33 +0100

    Add tests for filterFuncs.js module

--- public/env.js
+++ public/env.js
@@ -1,3 +1,3 @@
 window.env = {
-  API_KEY: 'ac39b316e0msh7d2b82002789670p10a6e9jsna343b2d68ca3',
+  "API_KEY": "ac39b316e0msh7d2b82002789670p10a6e9jsna343b2d68ca3"
 };
--- src/__test__/filterFuncs.test.js
+++ src/__test__/filterFuncs.test.js
@@ -1,3 +1,25 @@
 import getStandingsByFilter from "../logic/filterFuncs";
 
 const data = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+
+describe('getStandingsByFilter', () => {
+  test('returns indices 0 to 9, when Top Half filter is passed', () => {
+    expect(getStandingsByFilter(data, 'Top Half')).toEqual([1,2,3,4,5,6,7,8,9,10]);
+  });
+
+  test('returns indices 11 to 19, when Bottom Half filter is passed', () => {
+    expect(getStandingsByFilter(data, 'Bottom Half')).toEqual([11,12,13,14,15,16,17,18,19,20]);
+  });
+
+  test('returns indices 0 to 3, when Ucl Spot filter is passed', () => {
+    expect(getStandingsByFilter(data, 'Ucl Spot')).toEqual([1,2,3,4]);
+  });
+
+  test('returns indices 4 to 6, when Europa League Spot filter is passed', () => {
+    expect(getStandingsByFilter(data, 'Europa League Spot')).toEqual([5,6,7]);
+  });
+
+  test('returns indices 17 to 18, when Relegation zone filter is passed', () => {
+    expect(getStandingsByFilter(data, 'Relegation zone')).toEqual([18,19,20]);
+  });
+});
